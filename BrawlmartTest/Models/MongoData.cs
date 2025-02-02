0d59d11cda56f2b61b61d62e95388c0364567ad5
Joachim Carlsson <carlsson.joachim@gmail.com>
2025-02-02 21:35:30 +0100

    Added MongoDB connection that is meant to add some data each time a weapon is clicked on, storing User Interest for said item.

---
+++ BrawlmartTest/Models/MongoData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrawlmartTest.Models
+{
+    internal class MongoData
+    {
+        public Guid Id { get; set; }
+        public string ProductName { get; set; }
+        public int UserInterest { get; set; }
+    }
+}
---
+++ BrawlmartTest/MongoConnection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace BrawlmartTest
+{
+    internal class MongoConnection
+    {
+        private static MongoClient GetClient()
+        {
+            string connectionString = "mongodb+srv://joachimcarlsson:Admin1234@cluster0.rslea.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+            MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(connectionString));
+            settings.SslSettings = new SslSettings() { EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls13 };
+
+            var client = new MongoClient(settings);
+            return client;
+        }
+
+        public static IMongoCollection<Models.MongoData> CollectInterest()
+        {
+            var client = GetClient();
+            var database = client.GetDatabase("Brawlmart");
+            var interestCollection = database.GetCollection<Models.MongoData>("ProductInterest");
+            return interestCollection;
+        }
+    }
+}
